df9da73fc32ac96a8034c6d73774057ec874e40d
VladimirMakarov88 <makarovvm88@gmail.com>
2026-05-18 20:30:16 +0300

    Initial commit

---
+++ advertisements/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+
+
+class AdvertisementStatusChoices(models.TextChoices):
+    """Статусы объявления."""
+
+    OPEN = "OPEN", "Открыто"
+    CLOSED = "CLOSED", "Закрыто"
+
+
+class Advertisement(models.Model):
+    """Объявление."""
+
+    title = models.TextField()
+    description = models.TextField(default='')
+    status = models.TextField(
+        choices=AdvertisementStatusChoices.choices,
+        default=AdvertisementStatusChoices.OPEN
+    )
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    # добавляю счетовода открытых объявлений пользователем
+    @classmethod
+    def get_open_count_user(cls, user):
+        return cls.objects.filter(creator=user, status=AdvertisementStatusChoices.OPEN).count()
---
+++ advertisements/views.py
@@ -0,0 +1,24 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.permissions import IsOwnerOrReadOnly
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    ordering_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create"]:
+            return [IsAuthenticated()]
+        if self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
